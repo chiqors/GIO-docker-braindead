2885bfaa2ccb75a70218b8cc516f3012b58a9ba3
biosnod <biosnod@gmail.com>
2023-03-09 20:21:22 +0300

    Add CPU_USAGE_LIMIT, RELOAD_CONFIG_INTERVAL. Use OUTER_IP instead of 0.0.0.0 in docker-compose.yml.tmpl. Add Win support for CB3

--- cb01_live/dockerfiles/prepare-vars/prepare-vars.sh
+++ cb01_live/dockerfiles/prepare-vars/prepare-vars.sh
@@ -11,6 +11,9 @@ sed -i $'s/\r$//' ".env"
 source .env
 
 sed "s/%MYSQL_ROOT_PASSWORD%/$MYSQL_ROOT_PASSWORD/g" docker-compose.yml.tmpl > docker-compose.yml
+sed -i "s/%CPU_USAGE_LIMIT%/$CPU_USAGE_LIMIT/g" docker-compose.yml
+sed -i "s/%OUTER_IP%/$OUTER_IP/g" docker-compose.yml
+
 sed "s/%FLASK_SECRET_KEY%/$FLASK_SECRET_KEY/g" sdk/data/config.json.tmpl > sdk/data/config.json
 sed "s/%OUTER_IP%/$OUTER_IP/g" server/dbgate/conf/dbgate.xml.tmpl > server/dbgate/conf/dbgate.xml
 sed "s/%OUTER_IP%/$OUTER_IP/g" server/dispatch/conf/dispatch.xml.tmpl > server/dispatch/conf/dispatch.xml
@@ -20,3 +23,11 @@ sed "s/%OUTER_IP%/$OUTER_IP/g" server/muipserver/conf/muipserver.xml.tmpl > serv
 sed "s/%OUTER_IP%/$OUTER_IP/g" server/multiserver/conf/multiserver.xml.tmpl > server/multiserver/conf/multiserver.xml
 sed "s/%OUTER_IP%/$OUTER_IP/g" server/nodeserver/conf/nodeserver.xml.tmpl > server/nodeserver/conf/nodeserver.xml
 sed "s/%OUTER_IP%/$OUTER_IP/g" data.sql.tmpl > data.sql
+
+sed -i "s/%RELOAD_CONFIG_INTERVAL%/$RELOAD_CONFIG_INTERVAL/g" server/dbgate/conf/dbgate.xml
+sed -i "s/%RELOAD_CONFIG_INTERVAL%/$RELOAD_CONFIG_INTERVAL/g" server/dispatch/conf/dispatch.xml
+sed -i "s/%RELOAD_CONFIG_INTERVAL%/$RELOAD_CONFIG_INTERVAL/g" server/gameserver/conf/gameserver.xml
+sed -i "s/%RELOAD_CONFIG_INTERVAL%/$RELOAD_CONFIG_INTERVAL/g" server/gateserver/conf/gateserver.xml
+sed -i "s/%RELOAD_CONFIG_INTERVAL%/$RELOAD_CONFIG_INTERVAL/g" server/muipserver/conf/muipserver.xml
+sed -i "s/%RELOAD_CONFIG_INTERVAL%/$RELOAD_CONFIG_INTERVAL/g" server/multiserver/conf/multiserver.xml
+sed -i "s/%RELOAD_CONFIG_INTERVAL%/$RELOAD_CONFIG_INTERVAL/g" server/nodeserver/conf/nodeserver.xml
--- cb3update_live/bootstrap.sh
+++ cb3update_live/bootstrap.sh
@@ -1,66 +1,38 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v docker)" ]; then
-  echo 'E: Please install Docker and Compose plugin' >&2
-  exit 1
-fi
-
-if ! id -Gn | grep -qw 'docker'; then
-  echo 'E: Current user does not belong to "docker" group' >&2
-  exit
-fi
-
-if ! [ -x "$(command -v mysql)" ]; then
-  echo 'E: Please install MySQL/MariaDB _client_' >&2
-  exit 1
-fi
-
-# ---
-
 if [ -f .bootstrap.lock ]; then
-  echo 'E: Installation already finished; please delete ".bootstrap.lock" file if you wish to re-run it; note that it will _destroy_ current data in database!' >&2
+  echo 'Error: Installation already finished; please delete ".bootstrap.lock" file if you wish to re-run it; note that it will _destroy_ current data in database!' >&2
   exit 1
 fi
 
-# ---
+if [ -f docker-compose.yml ]; then
+  docker compose kill
+  docker compose rm -f
+fi
 
-read -p 'Outer IP: ' OUTER_IP
-MYSQL_ROOT_PASSWORD=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c16)
-FLASK_SECRET_KEY=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c16)
+docker compose kill
+docker compose rm
+sudo rm -rf mysql redis
 
-# ---
+# Remove all unused networks. Unused networks are those which are not referenced by any containers
+docker network prune -f
 
-sed "s/%MYSQL_ROOT_PASSWORD%/$MYSQL_ROOT_PASSWORD/g" docker-compose.yml.tmpl > docker-compose.yml
-sed "s/%FLASK_SECRET_KEY%/$FLASK_SECRET_KEY/g" sdk/data/config.json.tmpl > sdk/data/config.json
+docker compose -f docker-preinstall.yml up preparevars
+docker compose -f docker-preinstall.yml kill preparevars
+docker compose -f docker-preinstall.yml rm -f preparevars
 
-sed "s/%OUTER_IP%/$OUTER_IP/g" server/dbgate/conf/dbgate.xml.tmpl > server/dbgate/conf/dbgate.xml
-sed "s/%OUTER_IP%/$OUTER_IP/g" server/dispatch/conf/dispatch.xml.tmpl > server/dispatch/conf/dispatch.xml
-sed "s/%OUTER_IP%/$OUTER_IP/g" server/gameserver/conf/gameserver.xml.tmpl > server/gameserver/conf/gameserver.xml
-sed "s/%OUTER_IP%/$OUTER_IP/g" server/gateserver/conf/gateserver.xml.tmpl > server/gateserver/conf/gateserver.xml
-sed "s/%OUTER_IP%/$OUTER_IP/g" server/muipserver/conf/muipserver.xml.tmpl > server/muipserver/conf/muipserver.xml
-sed "s/%OUTER_IP%/$OUTER_IP/g" server/multiserver/conf/multiserver.xml.tmpl > server/multiserver/conf/multiserver.xml
-sed "s/%OUTER_IP%/$OUTER_IP/g" server/nodeserver/conf/nodeserver.xml.tmpl > server/nodeserver/conf/nodeserver.xml
-sed "s/%OUTER_IP%/$OUTER_IP/g" server/oaserver/conf/oaserver.xml.tmpl > server/oaserver/conf/oaserver.xml
-sed "s/%OUTER_IP%/$OUTER_IP/g" server/pathfindingserver/conf/pathfindingserver.xml.tmpl > server/pathfindingserver/conf/pathfindingserver.xml
+docker compose up -d mysql redis phpmyadmin adminer
 
-
-sed "s/%OUTER_IP%/$OUTER_IP/g" data.sql.tmpl > data.sql
-
-# ---
-
-docker compose up -d mysql redis
-
-while ! mysqladmin ping -h172.10.3.100 > /dev/null 2>&1; do
-  echo 'Waiting for MySQL to start up...'
-  sleep 3 # wait for mariadb to initialize itself
-done
-
-mysql -h172.10.3.100 -uroot -p$MYSQL_ROOT_PASSWORD < bootstrap.sql
-mysql -h172.10.3.100 -uhk4e_work -pmiHoYo2012 < data.sql
-
-# ---
+docker compose -f docker-preinstall.yml up preparedb
+docker compose -f docker-preinstall.yml kill preparedb
+docker compose -f docker-preinstall.yml rm -f preparedb
 
 touch .bootstrap.lock
-docker compose down
 
-echo 'Installation done. Execute "docker compose up -d" to begin; have fun!'
+docker compose down
+docker compose up -d
+
+echo 'Installation done. Servers starting...'
+echo 'If you want start servers in the future - DONT use bootstrap.sh!'
+echo 'Start it ONLY from Docker dashboard in Windows or using "docker compose up -d" in Linux/Windows/etc'
+echo 'Have fun!'
